30c0d793ae183ee63c1e01a8192d302ca44e6339
Roy Ge <rgecoder@gmail.com>
2019-09-12 16:34:36 -0700

    completed join video

--- queries/joins/joins_notes.sql
+++ queries/joins/joins_notes.sql
@@ -1,17 +1,33 @@
-SELECT * FROM clients
-JOIN billing ON billing.clients_id = clients_id
-ORDER BY clients.id;
 
-SELECT sum(amount) AS total,  count(*) AS num_transactions, clients.id, clients.first_name, clients.last_name, clients.email FROM billing
-JOIN clients on billing.clients_id = clients.id
+
+SELECT sites.domain_name, leads.first_name, leads.last_name
+FROM sites
+JOIN leads ON sites.id = leads.sites_id; 
+
+
+SELECT clients.first_name AS clients_first, clients.last_name, 
+sites.domain_name, leads.first_name AS leads_first
+FROM clients
+JOIN sites ON clients.id = sites.clients_id
+JOIN leads on sites.id = leads.sites_id; 
+
+SELECT clients.first_name, clients.last_name, sites.domain_name
+FROM clients
+LEFT JOIN sites ON clients.id = sites.clients_id;
+
+SELECT clients.first_name, clients.last_name, SUM(billing.amount)
+FROM clients
+JOIN billing ON clients.id = billing.clients_id
 GROUP BY clients.id;
 
-SELECT sum(amount) AS total,  YEAR(charged_datetime), count(*) AS num_transactions, clients.id, clients.first_name, clients.last_name, clients.email FROM billing 
-JOIN clients ON billing.clients_id = clients.id
-GROUP BY clients.id, YEAR(charged_datetime);
+SELECT clients.first_name, clients.last_name, GROUP_CONCAT('  ' ,  sites.domain_name) AS domains
+FROM clients
+JOIN sites ON clients.id = sites.clients_id
+GROUP BY clients.id;
 
-
-
-
+SELECT sites.domain_name, COUNT(leads.id)
+FROM sites
+JOIN leads ON sites.id = leads.sites_id
+GROUP BY sites.id;
 
 
